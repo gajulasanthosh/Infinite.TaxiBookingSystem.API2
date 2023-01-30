b0630e2eaade2402b9a4295151e138f7b0acffa8
SanthoshGajula <santhoshga@infinite.com>
2023-01-30 10:37:54 +0530

    Add project files.

---
+++ Infinite.TaxiBookingSystem.API/Models/User.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Infinite.TaxiBookingSystem.API.Models
+{
+    public class User : LoginModel
+    {
+        public int Id { get; set; }
+
+
+        public int EmployeeID { get; set; }
+
+        public int CustomerID { get; set; }
+
+        [Required]
+        public string Role { get; set; }
+
+        public Employee Employee { get; set; }
+        public Customer Customer { get; set; }
+
+        
+    }
+    public class LoginModel
+    {
+        [Required]
+        public string LoginID { get; set; }
+
+
+        [Required]
+        public string Password { get; set; }
+    }
+}
